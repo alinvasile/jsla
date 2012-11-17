ef54207fc2bc1a3d03f871ef520a5b9ba3829b12
alinvasile <alinachegalati@gmail.com>
2012-11-17 12:04:44 +0200

    Added missing files

---
+++ jsla-core/src/test/java/org/github/jsla/core/TestUserRate.java
@@ -0,0 +1,135 @@
+/*
+ * Copyright 2012 Alin Vasile
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.github.jsla.core;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.concurrent.TimeUnit;
+
+import org.github.jsla.core.authority.Authority;
+import org.github.jsla.core.monitor.NoRateDefinedException;
+import org.github.jsla.core.monitor.TransactionDeniedException;
+import org.github.jsla.core.monitor.TransactionMonitor;
+import org.github.jsla.core.monitor.TransactionMonitorService;
+import org.github.jsla.core.sla.Sla;
+import org.github.jsla.core.sla.SlaValue;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test class for user rates.
+ * 
+ * @author Alin Vasile
+ *
+ */
+public class TestUserRate {
+
+	private Authority authority;
+	
+	private Authority anonymous;
+	
+	private TransactionMonitorService service;
+	
+	@Before
+	public void setUp(){
+		authority = mock(Authority.class);
+		when(authority.getUsername()).thenReturn("alinvasile");
+		when(authority.getGroup()).thenReturn("gold");
+		when(authority.isAnonymous()).thenReturn(false);
+		
+		anonymous = mock(Authority.class);
+		when(anonymous.isAnonymous()).thenReturn(true);
+		
+		service = new TransactionMonitor();
+	}
+	
+	@After
+	public void tearDown(){
+		service = null;
+	}
+	
+	@Test(expected=NoRateDefinedException.class)
+	public void testNoUserSlaRate(){
+		Sla sla = Sla.createSlaRateWithQuotaUnlimited(new SlaValue(1, 1, TimeUnit.SECONDS)); 
+		
+		service.addUsernameConstraint("nobody", sla);
+		service.addGroupConstraint("nobody", sla);
+		
+		service.grant(authority);
+	}
+	
+	@Test()
+	public void testUsernameSlaRate(){
+		Sla sla = Sla.createSlaRateWithQuotaUnlimited(new SlaValue(1, 1, TimeUnit.SECONDS)); 
+		
+		service.addUsernameConstraint(authority.getUsername(), sla);
+		service.addGroupConstraint("nobody", sla);
+		
+		service.grant(authority);
+	}
+	
+	@Test(expected=TransactionDeniedException.class)
+	public void testUsernameSlaRateExceeded(){
+		Sla sla = Sla.createSlaRateWithQuotaUnlimited(new SlaValue(1, 1, TimeUnit.SECONDS)); 
+		
+		service.addUsernameConstraint(authority.getUsername(), sla);
+		
+		service.grant(authority);
+		service.grant(authority);
+	}
+	
+	@Test()
+	public void testGroupSlaRate(){
+		Sla sla = Sla.createSlaRateWithQuotaUnlimited(new SlaValue(1, 1, TimeUnit.SECONDS)); 
+		
+		service.addGroupConstraint(authority.getGroup(), sla);
+		
+		service.grant(authority);
+	}
+	
+	@Test(expected=TransactionDeniedException.class)
+	public void testGroupSlaRateExceeded(){
+		Sla sla = Sla.createSlaRateWithQuotaUnlimited(new SlaValue(1, 1, TimeUnit.SECONDS)); 
+		
+		service.addGroupConstraint(authority.getGroup(), sla);
+		
+		service.grant(authority);
+		service.grant(authority);
+	}
+	
+	@Test()
+	public void testAnonymousSlaRate(){
+		Sla sla = Sla.createSlaRateWithQuotaUnlimited(new SlaValue(1, 1, TimeUnit.SECONDS)); 
+		
+		service.addAnonymousConstraint(sla);
+		
+		service.grant(anonymous);
+	}
+	
+	@Test(expected=TransactionDeniedException.class)
+	public void testAnonymousSlaRateExceeded(){
+		Sla sla = Sla.createSlaRateWithQuotaUnlimited(new SlaValue(1, 1, TimeUnit.SECONDS)); 
+		
+		service.addAnonymousConstraint(sla);
+		
+		service.grant(anonymous);
+		service.grant(anonymous);
+	}
+	
+	
+}
