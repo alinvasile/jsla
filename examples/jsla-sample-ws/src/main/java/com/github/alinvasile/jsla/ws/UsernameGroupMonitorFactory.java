228a59a3af165d1d56df6b7fcd4b55445a56e008
alinvasile <alinachegalati@gmail.com>
2013-01-07 15:56:22 +0200

    Minor changes for test-ws
    Signed-off-by: alinvasile <alinachegalati@gmail.com>

--- examples/jsla-sample-ws/src/main/java/com/github/alinvasile/jsla/ws/UsernameGroupMonitorFactory.java
+++ examples/jsla-sample-ws/src/main/java/com/github/alinvasile/jsla/ws/UsernameGroupMonitorFactory.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.github.alinvasile.jsla.ws.jee5;
+package com.github.alinvasile.jsla.ws;
 
 import java.util.concurrent.TimeUnit;
 
